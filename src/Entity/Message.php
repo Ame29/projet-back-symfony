78329b124bdf2ac04bf0f86b7ba2bea8a64e44e2
Ame29 <37901772+Ame29@users.noreply.github.com>
2022-01-09 23:21:56 +0100

    Add files via upload

---
+++ src/Entity/Message.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"messages"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"messages"})
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"messages"})
+     */
+    private $datepublication;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Groups({"messages"})
+     */
+    private $message;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="messages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDatepublication(): ?\DateTimeInterface
+    {
+        return $this->datepublication;
+    }
+
+    public function setDatepublication(\DateTimeInterface $datepublication): self
+    {
+        $this->datepublication = $datepublication;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Category
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Category $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
